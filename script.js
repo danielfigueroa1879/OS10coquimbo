e07c50f30e5c7ca2672109e4eaace1fb83ca2ff8
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-06-05 00:20:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -508,7 +508,7 @@ async function generarReporte(sectionId) {
                 } else if (data.cell.text[0] === 'No Cumple') {
                     // MODIFICADO: Cambié el naranja [243, 156, 18] por rojo [255, 186, 210]
                     data.cell.styles.fillColor = [247, 206, 205]; // ROJO para No Cumple
-                    data.cell.styles.textColor = [247, 49, 5];
+                    data.cell.styles.textColor = [247, 49, 8];
                 }
             }
             // ✅ ASEGURAR TEXTO NEGRO EN TODAS LAS OTRAS CELDAS (sin negritas)
