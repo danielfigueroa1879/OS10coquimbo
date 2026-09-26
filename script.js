7668930b5bea4ca2a8f9ad8a48e97eea22b6f1f4
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2026-09-26 13:17:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const requisitosData = {
     ],
     'directiva-funcionamiento': {
     'instalacion': [
-        { id: 1, text: 'SOLICITUD SIMPLE ante Subsecretaría de Prevención del Delito (De la entidad empleadora o empresa RR.HH.)' },
+        { id: 1, text: '<strong> SOLICITUD SIMPLE </strong> ante Subsecretaría de Prevención del Delito (De la entidad empleadora o empresa RR.HH.)' },
         { id: 2, text: 'PRESENTACIÓN CON 15 DÍAS DE ANTICIPACIÓN al inicio del servicio (Art. 92 inciso 6° D.209)' },
         { id: 3, text: 'LISTADO DE LOS GG.SS. con individualización completa <br> (Art. 50 N° 1 y 92 inc. 2 D.209)' },
         { id: 4, text: 'CURSO DE FORMACIÓN: Mínimo 90 horas pedagógicas aprobado (Art. 89 N°1 D.209)' },
